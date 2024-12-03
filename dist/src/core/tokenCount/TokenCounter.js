9968c2e6ea76e2a71cec640751a87245d96735b7
johnson.chen <joy6677@qq.com>
2024-12-03 22:07:11 +0800

    Update: dist/src/core/tokenCount/TokenCounter.js ( 1 file changed, 8 deletions(-))

--- dist/src/core/tokenCount/TokenCounter.js
+++
@@ -1,8 +0,0 @@
-class TokenCounter {
-    countTokens(text) {
-        const tokens = text.split(/\s+/);
-        return tokens.length;
-    }
-}
-export {};
-//# sourceMappingURL=TokenCounter.js.map
