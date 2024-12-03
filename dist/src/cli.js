a4f26fb94bbacc8b4be2a627c8392ab85cb2c4cd
johnson.chen <joy6677@qq.com>
2024-12-03 22:04:06 +0800

    Update: dist/src/cli.js ( 1 file changed, 7 deletions(-))

--- dist/src/cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { run } from './cli/cliRun.js';
-run().catch((error) => {
-    console.error('Error:', error);
-    process.exit(1);
-});
-//# sourceMappingURL=cli.js.map
